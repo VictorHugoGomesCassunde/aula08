828d87190fd40da5d419c4b0f6ce8ad14db1cf77
VictorHugoGomesCassunde <victorhugocassunde10@gmail.com>
2024-11-25 14:34:33 -0400

    25/11
    terminar

--- cliente/src/pages/Registro.jsx
+++ cliente/src/pages/Registro.jsx
@@ -1,9 +1,13 @@
 import { useState } from "react";
 
 export default function Registrar() {
+  const[nome,setNome] = useState("");
+  const[email,setEmail] = useState("");
 
-
+  const registrar = (event) => 
   return (
-        
+        <main>
+          <
+        </main>
   );
 }
