fd2fae04d0efedc01f43db230f756de1ce8842de
VictorHugoGomesCassunde <victorhugocassunde10@gmail.com>
2024-12-02 14:21:16 -0400

    02/12/24
    arrumar

--- cliente/src/pages/Registro.jsx
+++ cliente/src/pages/Registro.jsx
@@ -4,10 +4,32 @@ export default function Registrar() {
   const[nome,setNome] = useState("");
   const[email,setEmail] = useState("");
 
-  const registrar = (event) => 
-  return (
-        <main>
-          <
-        </main>
-  );
-}
+  const registrar = async (event) =>{
+    event.preventDefault();
+    try{
+      const resposta = await fetch('http://localhost:3000/usuarios', {
+      method:'POST',
+      headers: { 'Content-Type':'Application/json'},
+      body: JSON.stringify({
+        nome: nome,
+        email: email
+      })
+    });
+        if (resposta.ok) {
+          NavigationPreloadManager('/');
+        }
+      }catch (err) {
+        alert('Erro no registro', err)
+      }
+      }
+return(
+<>
+<form onSubmit={registro}>
+  <input type="text" name="" id="" onChange=(event) => setNome(event)
+</form>
+</>
+)
+    }
+  
+  
+      
