b6470fd8dd86b03332c423ecb23b999bf28a8ceb
VictorHugoGomesCassunde <victorhugocassunde10@gmail.com>
2025-01-23 16:27:52 -0400

    23/01
    terminar

--- cliente/src/pages/Home.jsx
+++ cliente/src/pages/Home.jsx
@@ -30,43 +30,70 @@ export default function Home() {
   }
  };
 
-    const exportarPDF = () => {
-      const doc = new jsPDF
+ const exportarPDF = () => {
+  const doc = new jsPDF();
 
-      const tabela = usuarios.map(usuario => [
-        usuario.id,
-        usuario.nome,
-        usuario.email
-      ]);
-      doc.text("Lista de Usuários",10, 10);
+  const tabela = usuarios.map(usuario => [
+    usuario.id,
+    usuario.nome,
+    usuario.email,
+  ]);
 
-      doc.autoTble({
-        head:[[ "ID", "Nome", "E-mail"]],
-        body: tabela
-      });
-      doc.save("alunosIFMS");
-    }
+  doc.text("Lista de Usuários", 10, 10); // Título do PDF
+
+  doc.autoTable({
+    head: [["ID", "Nome", "E-mail"]], // Cabeçalho da tabela
+    body: tabela,                    // Dados da tabela
+    
+  });
+  const nomeInput = document.getElementById("nomeInput")?.value || "Não informado";
+  const emailInput = document.getElementById("emailInput")?.value || "Não informado";
+
+  doc.text("Informações do usuario:", 10, doc.lastAutoTable.finalY + 10); // Adiciona após a tabela
+  doc.text(`Nome: ${nomeInput}`, 10, doc.lastAutoTable.finalY + 20);
+  doc.text(`E-mail: ${emailInput}`, 10, doc.lastAutoTable.finalY + 30);
+
+
+  doc.save("usuarios.pdf"); // Nome do arquivo PDF
+};
+
     return (
+      
       <div>
         <Button variant="contained" onClick={()=> exportarPDF()}>
           Gerar PDF
         </Button>
-    <table>
-      <tr>
-        <td>Nome</td>
-        <td>E-mail</td>
-      </tr>
-      {usuarios.map((usuario) =>
-        <tr key={usuario.id}>
-          <td>{usuario.nome}</td>
-          <td>{usuario.email}</td>
-          <td> <button onClick={() => removerPessoa(usuario.id)} > X </button> </td>
+
+        <table>
+  <thead>
+    <tr>
+      <th>Nome</th>
+      <th>E-mail</th>
+    </tr>
+
+    <div>
+  <input id="nomeInput" type="nome" placeholder="Digite o Nome" />
+  <input id="emailInput" type="email" placeholder="Digite o E-mail" />
+</div>
+
+  </thead>
+  
+  <tbody>
+    {usuarios.map((usuario) => (
+      <tr key={usuario.id}>
+        <td>{usuario.nome}</td>
+        <td>{usuario.email}</td>
+        <td>
+          <button onClick={() => removerPessoa(usuario.id)}>Excluir</button>
           <Link to={'/alterar/' + usuario.id}>
             <button>Alterar</button>
           </Link>
-        </tr>
-      )}
-    </table>
+          
+        </td>
+      </tr>
+    ))}
+  </tbody>
+</table>
     </div>
   );
 }
